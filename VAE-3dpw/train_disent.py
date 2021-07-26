12765a5dbb970e2817adb5c2e78b183e67c3c9d9
Behnam Parsaeifard <parsaeif@izar.ib0.cm.izar.local>
2021-07-26 18:52:01 +0200

    disentangled & non-disentangled VAE (3dpw)

---
+++ VAE-3dpw/train_disent.py
@@ -0,0 +1,342 @@
+import torch 
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import time
+#import cv2
+import utils
+
+from PIL import Image, ImageDraw
+from mpl_toolkits.axes_grid1 import ImageGrid
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import random
+import json
+import viz
+import args_module
+
+args = args_module.args()
+torch.manual_seed(0)
+random.seed(0)
+np.random.seed(0)
+
+
+def visu(doc, obs_p, pred_p, target_p, obs_f, target_f):
+
+    #g = torch.Generator()
+    #g.manual_seed(epoch)
+    for i in range(10):
+       #i = torch.randint(low=0, high=obs_p.shape[0]-1, size=(1,), generator=g).item()
+       viewer = viz.Seq3DPose(window=1.0)
+       prefix='result_'+doc+'/scenario_{:02d}/'.format(i)
+       seq_in = obs_p[i]
+       viewer.center(seq_in[..., 4:6, 0].mean(), seq_in[..., 4:6, 1].mean(), seq_in[..., 4:6, 2].mean())
+       viewer.view(seq_in, 'Input', prefix=prefix)
+       seq_ex = pred_p[i]
+       viewer.view(seq_ex, 'Prediction', prefix=prefix)
+       seq_exe = target_p[i]
+       viewer.view(seq_exe, 'GroundTruth', prefix=prefix)
+
+       print("deb: ", pred_p[i].shape, target_p[i].shape)
+       vim = utils.myVIM(pred_p[i].view(2,14,39).permute(1,0,2), target_p[i].view(2,14,39).permute(1,0,2))
+       with open('result_'+doc+'/scenario_{:02d}/'.format(i)+"info", 'w') as f:
+           f.write("vim: {}\n".format(vim/2))
+           lst = " ".join(obs_f[i])
+           f.write("ObservationFrames: {}\n".format(lst))
+           lst = " ".join(target_f[i])
+           f.write("FutureFrames: {}\n".format(lst))
+
+def mse(pred, true):
+    seq_len, batch, l = pred.shape 
+    #return torch.mean(torch.sqrt(torch.sum((pred-true)**2, dim=-1)/13))
+    return torch.mean((pred-true)**2)
+
+VISUALIZE = False #True
+MODEL_VERSION = 'avg_hip' 
+#MODEL_VERSION = 'neck' 
+#MODEL_VERSION = 'rankle' #right ankle 
+
+#mse = nn.MSELoss()
+#l1e = nn.L1Loss()
+#l1e = nn.MSELoss()
+bce = nn.BCELoss()
+train_s_scores = []
+train_pose_scores=[]
+val_pose_scores=[]
+train_c_scores = []
+val_s_scores   = []
+val_c_scores   = []
+
+import DataLoader
+train = DataLoader.data_loader(args)
+args.dtype = 'valid'
+args.save_path = args.save_path.replace('train', 'val')
+args.file = args.file.replace('train', 'val')
+#val = DataLoader.data_loader(args)
+val_all = DataLoader.data_loader_val(args)
+
+import DataLoader_test
+test = DataLoader_test.data_loader_test()
+
+best_model_val = 100000.
+
+import model_vae 
+net_g = model_vae.LSTM_local_global().to(args.device).double()
+encoder = model_vae.Encoder(pose_dim=39, h_dim=32, latent_dim=16)
+decoder = model_vae.Decoder(pose_dim=39, h_dim=32, latent_dim=16)
+net_l = model_vae.VAE(Encoder=encoder, Decoder=decoder)
+if torch.cuda.is_available():
+    net_l.cuda()
+net_l.double()
+
+optimizer = optim.Adam( list(net_l.parameters()) + list(net_g.parameters()), lr=0.004)
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.7, patience=35, threshold = 1e-8, verbose=True)
+coeff = 0.2
+print('='*100)
+print('Training ...')
+
+for epoch in range(args.n_epochs):
+    start = time.time()
+    
+    ade_train  = 0
+    fde_train  = 0
+    vim_train = 0
+    loss_train = 0
+
+    counter = 0
+    net_l.train()
+    net_g.train()
+    for idx, (obs_p, obs_s, obs_f, target_p, target_s, target_f, start_end_idx) in enumerate(train):
+
+        batch = obs_p.size(1)
+        counter += batch 
+        obs_p = obs_p.to(device='cpu').double()
+        obs_s = obs_s.to(device='cpu').double()
+        target_p = target_p.to(device='cpu').double()
+        target_s = target_s.to(device='cpu').double()
+        
+        ###########################################################
+        #########splitting the motion into local + global##########
+        ###########################################################
+        if(MODEL_VERSION == 'avg_hip'):
+            obs_s_g = 0.5*(obs_s.view(15, batch, 13, 3)[:,:,0] + obs_s.view(15, batch, 13, 3)[:,:,1])
+            target_s_g = 0.5*(target_s.view(14, batch, 13, 3)[:,:,0] + target_s.view(14, batch, 13, 3)[:,:,1])
+        elif(MODEL_VERSION == 'neck'):
+            obs_s_g = obs_s.view(15, batch, 13, 3)[:,:,6]
+            target_s_g = target_s.view(14, batch, 13, 3)[:,:,6]
+        elif(MODEL_VERSION == 'rankle'):
+            obs_s_g = obs_s.view(15, batch, 13, 3)[:,:,4]
+            target_s_g = target_s.view(14, batch, 13, 3)[:,:,4]
+        else:
+            print("ERROR")
+            exit()
+        ###########################################################
+
+        net_g.zero_grad()
+        ###########################################################
+        #####predicting the global speed and calculate mse loss####
+        ###########################################################
+        speed_preds_g = net_g(global_s=obs_s_g)
+        loss_g = mse(speed_preds_g, target_s_g)
+        ###########################################################
+        
+        net_l.zero_grad()
+        ########################################################################
+        ######predicting the local speed using VAE and calculate loss########### 
+        ########################################################################
+        obs_s_l = (obs_s.view(15, batch, 13, 3) - obs_s_g.view(15, batch, 1, 3)).view(15, batch, 39)
+        output, mean, log_var = net_l(obs_s_l.double())
+        target_s_l = (target_s.view(14, batch, 13, 3) - target_s_g.view(14, batch, 1, 3)).view(14, batch, 39)
+        loss_l = model_vae.vae_loss_function(target_s_l.double(), output, mean, log_var) #- 0.0001* torch.norm(output)
+        ########################################################################
+
+        ###########################################################
+        speed_preds = (speed_preds_g.view(14, batch, 1, 3) + output.view(14, batch, 13, 3)).view(14, batch, 39)
+        preds_p = utils.speed2pos(speed_preds, obs_p) 
+        ###########################################################
+
+        #####################total loss############################
+        #version 1: disentangled
+        loss = loss_g + coeff*loss_l #+ 0.0001*mse(speed_preds, target_s)
+        #version 2: 
+        #loss = mse(speed_preds, target_s)
+        #########################debug#############################
+        if(epoch%10 == 0): print(f"deb: loss_g {loss_g.item()} loss_l {coeff*loss_l.item()}")
+        ####################backward and optimize##################
+        loss.backward()
+        ###########################################################
+        optimizer.step()
+
+        #####################calculating the metrics########################
+        ade_train += float(utils.ADE_c(preds_p, target_p))
+        fde_train += float(utils.FDE_c(preds_p, target_p))
+        vim_train += utils.myVIM(preds_p, target_p)
+        loss_train += loss.item()*batch
+        ####################################################################
+
+    loss_train /= counter
+    ade_train  /= counter
+    fde_train  /= counter    
+    vim_train  /= counter    
+    scheduler.step(loss_train)
+ 
+    acc_val = 0
+    ade_val  = 0
+    fde_val  = 0
+    vim_val = 0
+    loss_val = 0
+
+    counter = 0
+    net_l.eval()
+    net_g.eval()
+    for idx, (obs_p, obs_s, obs_f, target_p, target_s, target_f, start_end_idx) in enumerate(val_all):
+
+        batch = obs_p.size(1) 
+        counter += batch 
+        obs_p = obs_p.to(device='cpu').double()
+        obs_s = obs_s.to(device='cpu').double()
+        target_p = target_p.to(device='cpu').double()
+        target_s = target_s.to(device='cpu').double()
+        
+        with torch.no_grad():
+
+            ###########################################################
+            #########splitting the motion into local + global##########
+            ###########################################################
+            if(MODEL_VERSION == 'avg_hip'):
+                obs_s_g = 0.5*(obs_s.view(15, batch, 13, 3)[:,:,0] + obs_s.view(15, batch, 13, 3)[:,:,1])
+                target_s_g = 0.5*(target_s.view(14, batch, 13, 3)[:,:,0] + target_s.view(14, batch, 13, 3)[:,:,1])
+            elif(MODEL_VERSION == 'neck'):
+                obs_s_g = obs_s.view(15, batch, 13, 3)[:,:,6]
+                target_s_g = target_s.view(14, batch, 13, 3)[:,:,6]
+            elif(MODEL_VERSION == 'rankle'):
+                obs_s_g = obs_s.view(15, batch, 13, 3)[:,:,4]
+                target_s_g = target_s.view(14, batch, 13, 3)[:,:,4]
+            else:
+                print("ERROR")
+                exit()
+            ###########################################################
+
+            ###########################################################
+            #####predicting the global speed and calculate mse loss####
+            ###########################################################
+            speed_preds_g = net_g(global_s=obs_s_g)
+            loss_g = mse(speed_preds_g, target_s_g)
+            ###########################################################
+
+            ########################################################################
+            ######predicting the local speed using VAE and calculate loss########### 
+            ########################################################################
+            obs_s_l = (obs_s.view(15, batch, 13, 3) - obs_s_g.view(15, batch, 1, 3)).view(15, batch, 39)
+            output, mean, log_var = net_l(obs_s_l.double())
+            target_s_l = (target_s.view(14, batch, 13, 3) - target_s_g.view(14, batch, 1, 3)).view(14, batch, 39)
+            loss_l = model_vae.vae_loss_function(target_s_l.double(), output, mean, log_var)
+            ########################################################################
+
+            ###########################################################
+            speed_preds = (speed_preds_g.view(14, batch, 1, 3) + output.view(14, batch, 13, 3)).view(14, batch, 39)
+            preds_p = utils.speed2pos(speed_preds, obs_p) 
+            ###########################################################
+
+            #####################total loss############################
+            #version 1: disentangled
+            loss = loss_g + coeff*loss_l
+            #version 2:
+            #loss = mse(speed_preds, target_s)
+            ###########################################################
+        
+            #####################calculating the metrics########################
+            ade_val += float(utils.ADE_c(preds_p, target_p))
+            fde_val += float(utils.FDE_c(preds_p, target_p))
+            vim_val += utils.myVIM(preds_p, target_p)
+            loss_val += loss.item()*batch
+            ####################################################################
+
+
+
+        ########################write val predictions to file##########################
+        obs_p_val = obs_p
+        target_p_val = target_p
+        preds_p_val = preds_p
+        start_end_idx_val = start_end_idx
+        obs_f_val = obs_f
+        target_f_val = target_f
+        ###############################################################################
+ 
+    loss_val /= counter
+    ade_val  /= counter
+    fde_val  /= counter    
+    vim_val  /= counter    
+
+    print("e: %d "%epoch,
+          "|loss_t: %0.6f "%loss_train,
+          "|loss_v: %0.6f "%loss_val,
+          "|fde_t: %0.6f "%fde_train,
+          "|fde_v: %0.6f "%fde_val,
+          "|ade_t: %0.6f "%ade_train, 
+          "|ade_v: %0.6f "%ade_val, 
+          "|vim_t: %0.6f "%vim_train,
+          "|vim_v: %0.6f "%vim_val) 
+
+    ########################write test predictions to file##########################
+    if(vim_val < best_model_val):
+        best_model_val = vim_val 
+
+        print(f" \n saving the best model with vim_train {vim_train} and vim_val {vim_val} \n")
+        ######################saving the checkpoints######################
+        torch.save(net_g.state_dict(), 'checkpoint_g.pkl')
+        torch.save(net_l.state_dict(), 'checkpoint_l.pkl')
+
+        net_l.eval()
+        net_g.eval()
+        for idx, (obs_p, obs_s, obs_f, target_f, start_end_idx) in enumerate(test):
+
+            obs_p = obs_p.to(device='cpu').double()
+            obs_s = obs_s.to(device='cpu').double()
+            
+            batch = obs_p.size(1)
+            ###########################################################
+            #########splitting the motion into local + global##########
+            ###########################################################
+            if(MODEL_VERSION == 'avg_hip'):
+                obs_s_g = 0.5*(obs_s.view(15, batch, 13, 3)[:,:,0] + obs_s.view(15, batch, 13, 3)[:,:,1])
+            elif(MODEL_VERSION == 'neck'):
+                obs_s_g = obs_s.view(15, batch, 13, 3)[:,:,6]
+            elif(MODEL_VERSION == 'rankle'):
+                obs_s_g = obs_s.view(15, batch, 13, 3)[:,:,4]
+            else:
+                print("ERROR")
+                exit()
+            ###########################################################
+
+
+            with torch.no_grad():
+                obs_s_l = (obs_s.view(15, batch, 13, 3) - obs_s_g.view(15, batch, 1, 3)).view(15, batch, 39)
+                pred_s_g = net_g(global_s=obs_s_g)
+                output, mean, log_var = net_l(obs_s_l.double())
+                speed_preds = (pred_s_g.view(14, batch, 1, 3) + output.view(14, batch, 13, 3)).view(14, batch, 39)
+                preds_p = utils.speed2pos(speed_preds, obs_p) 
+
+        #preds_p => (14, 170, 39) => alist => (85, 2, 14, 39) 
+        alist = []
+        for _, (start, end) in enumerate(start_end_idx):
+            alist.append(preds_p.permute(1,0,2)[start:end].tolist())
+        with open('3dpw_predictions.json', 'w') as f:
+            f.write(json.dumps(alist))
+        #(85 2 14 39)
+
+        alist = []
+        for _, (start, end) in enumerate(start_end_idx_val):
+            alist.append(preds_p_val.permute(1,0,2)[start:end].tolist())
+        with open('3dpw_predictions_val.json', 'w') as f:
+            f.write(json.dumps(alist))
+
+        #########################visualization of results##############################
+        if(vim_val < 0.155):
+            visu("disentangledVAE", obs_p_val.permute(1,0,2).view(-1, 2, 16, 13, 3), preds_p_val.permute(1,0,2).view(-1, 2, 14, 13, 3), target_p_val.permute(1,0,2).view(-1, 2, 14, 13, 3), obs_f_val, target_f_val)
+        ###############################################################################
+
+print('='*100) 
+print('Saving ...')
+print('Done !')
---
+++ VAE-3dpw/viz.py
@@ -0,0 +1,171 @@
+"""
+    Functions to visualize human poses
+    adapted from https://github.com/una-dinosauria/human-motion-prediction/blob/master/src/viz.py
+"""
+
+import os
+import numpy as np
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+
+
+class Ax3DPose(object):
+    def __init__(self, ax, window=1.0, lcolor="#3498db", rcolor="#e74c3c"):
+        """
+        Create a 3d pose visualizer that can be updated with new poses.
+
+        Args
+            ax: 3d axis to plot the 3d pose on
+            lcolor: String. Colour for the left part of the body
+            rcolor: String. Colour for the right part of the body
+        """
+
+        # Start and endpoints of our representation
+        self.I =  np.array([0, 0, 2, 1, 3, 7, 8, 7, 6, 6, 7, 9, 8, 10])
+        self.J =  np.array([1, 2, 4, 3, 5, 0, 1, 8, 7, 8, 9, 11, 10, 12])
+
+        # Left / right indicator
+        self.LR = np.array([0, 1, 1, 2, 2, 1, 2, 0, 1, 2, 1, 1, 2, 2], dtype=np.int8)
+        self.color = {0: "k", 1:lcolor, 2:rcolor}
+
+        self.ax = ax
+        self.num_person = 2
+
+        vals = np.zeros((self.num_person, 28, 3))
+
+        # Make connection matrix
+        self.plots = []
+        for p in range(self.num_person):
+            for i in np.arange( len(self.I) ):
+                x = np.array( [vals[p, self.I[i], 0], vals[p, self.J[i], 0]] )
+                y = np.array( [vals[p, self.I[i], 1], vals[p, self.J[i], 1]] )
+                z = np.array( [vals[p, self.I[i], 2], vals[p, self.J[i], 2]] )
+                color = 'k'
+                self.plots.append(self.ax.plot(x, y, z, lw=2, c=self.color[self.LR[i]]))
+
+        self.ax.set_xlabel("x")
+        self.ax.set_ylabel("y")
+        self.ax.set_zlabel("z")
+        self.ax.view_init(15, -65)
+
+        self.window = window
+        self.center()
+
+        self.ax.set_xlim3d([-self.window+self.xc, self.window+self.xc])
+        self.ax.set_ylim3d([-self.window-self.zc, self.window-self.zc])
+        self.ax.set_zlim3d([-0.2 + self.yc, self.window*2 + 0.2 + self.yc])
+
+    def center(self, xc=0.0, yc=0.0, zc=0.0):
+        self.xc = xc
+        self.yc = yc
+        self.zc = zc    
+        # print('Centered at: {:.2f}, {:.2f}, {:.2f}'.format(xc, yc, zc))
+
+    def update(self, poses):
+        """
+        Update the plotted 3d pose.
+
+        Args
+          poses: [2, 13, 3] np array. The pose to plot.
+        Returns
+          Nothing. Simply updates the axis with the new pose.
+        """
+
+        assert poses.shape[0] == self.num_person, "data should be about 2 persons, it has %d instead" % poses.shape[0]
+
+        for p in range(poses.shape[0]):
+            for i in np.arange( len(self.I) ):
+                x = np.array( [poses[p, self.I[i], 0], poses[p, self.J[i], 0]] )
+                y = np.array( [poses[p, self.I[i], 1], poses[p, self.J[i], 1]] )
+                z = np.array( [poses[p, self.I[i], 2], poses[p, self.J[i], 2]] )
+                self.plots[i+p*len(self.I)][0].set_xdata(x)
+                self.plots[i+p*len(self.I)][0].set_ydata(-z)
+                self.plots[i+p*len(self.I)][0].set_3d_properties(y)
+                self.plots[i+p*len(self.I)][0].set_color(self.color[self.LR[i]])
+
+        self.ax.set_xlim3d([-self.window+self.xc, self.window+self.xc])
+        self.ax.set_ylim3d([-self.window-self.zc, self.window-self.zc])
+        self.ax.set_zlim3d([-0.2 + self.yc, self.window*2 + 0.2 + self.yc])        
+        # self.ax.set_zlim3d([self.yc -0.2 - self.window, self.yc + self.window + 0.2])
+        self.ax.set_box_aspect((1, 1, 1))
+
+
+class Seq3DPose(object):
+    """
+    Create a 3d pose squence visualizer that can be updated with new frames.
+    """
+
+    def __init__(self, window=1.0):
+        self.fig = plt.figure(figsize=(6, 6))
+        ax = plt.axes(projection='3d')
+        self.ob = Ax3DPose(ax, window)
+
+    def center(self, xc, yc, zc):
+        self.ob.center(xc, yc, zc)
+
+    def view(self, seq, name=None, prefix=None):
+        for frame in range(seq.shape[1]):
+            poses = seq[:, frame]
+            self.ob.update(poses)
+            self.fig.show()
+            self.ob.ax.set_title('{} #{:d}/{:d}'.format(name, frame+1, seq.shape[1]))
+            self.fig.canvas.draw()
+            if prefix:
+                self.fig.savefig(prefix + '_' + name + '_{:d}.png'.format(frame), bbox_inches='tight')
+            else:
+                self.fig.pause(0.1)
+
+class CameraPose:
+    def __init__(self):
+        self.fig = plt.figure(figsize=(6, 6))
+        self.ax = plt.axes(projection='3d')
+        # self.ax.set_aspect("auto")
+        self.ax.set_xlabel('x')
+        self.ax.set_ylabel('y')
+        self.ax.set_zlabel('z')
+        self.window = 0.1
+
+    def center(self, centroid):
+        self.xc = centroid[0]
+        self.yc = centroid[1]
+        self.zc = centroid[2]
+
+    def extrinsic2pyramid(self, extrinsic, color='r', focal_len_scaled=5, aspect_ratio=0.3):
+        vertex_std = np.array([[0, 0, 0, 1],
+                               [focal_len_scaled * aspect_ratio, -focal_len_scaled * aspect_ratio, focal_len_scaled, 1],
+                               [focal_len_scaled * aspect_ratio, focal_len_scaled * aspect_ratio, focal_len_scaled, 1],
+                               [-focal_len_scaled * aspect_ratio, focal_len_scaled * aspect_ratio, focal_len_scaled, 1],
+                               [-focal_len_scaled * aspect_ratio, -focal_len_scaled * aspect_ratio, focal_len_scaled, 1]])
+        vertex_transformed = vertex_std @ extrinsic.T
+        meshes = [[vertex_transformed[0, :-1], vertex_transformed[1][:-1], vertex_transformed[2, :-1]],
+                            [vertex_transformed[0, :-1], vertex_transformed[2, :-1], vertex_transformed[3, :-1]],
+                            [vertex_transformed[0, :-1], vertex_transformed[3, :-1], vertex_transformed[4, :-1]],
+                            [vertex_transformed[0, :-1], vertex_transformed[4, :-1], vertex_transformed[1, :-1]],
+                            [vertex_transformed[1, :-1], vertex_transformed[2, :-1], vertex_transformed[3, :-1], vertex_transformed[4, :-1]]]
+        self.ax.add_collection3d(
+            Poly3DCollection(meshes, facecolors=color, linewidths=0.3, edgecolors=color, alpha=0.35))
+
+        self.ax.set_xlim([self.xc-self.window, self.xc+self.window])
+        self.ax.set_ylim([self.yc-self.window, self.yc+self.window])
+        self.ax.set_zlim([self.zc-self.window, self.zc+self.window])
+        self.ax.set_box_aspect((1, 1, 1))
+
+    # def customize_legend(self, list_label):
+    #     list_handle = []
+    #     for idx, label in enumerate(list_label):
+    #         color = plt.cm.rainbow(idx / len(list_label))
+    #         patch = Patch(color=color, label=label)
+    #         list_handle.append(patch)
+    #     plt.legend(loc='right', bbox_to_anchor=(1.8, 0.5), handles=list_handle)
+
+    def colorbar(self, max_frame_length):
+        cmap = mpl.cm.rainbow
+        norm = mpl.colors.Normalize(vmin=0, vmax=max_frame_length)
+        self.fig.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=cmap), orientation='vertical', label='Frame Number')
+
+    def show(self):
+        plt.title('Extrinsic Parameters')
+        plt.show()
