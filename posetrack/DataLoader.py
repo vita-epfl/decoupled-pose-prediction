235049921f47e975ba8e0302bfb8e37b46720bd5
behnam <behnam.parsaeifard@gmail.com>
2021-08-14 05:24:06 +0430

    posetrack added

---
+++ posetrack/DataLoader.py
@@ -0,0 +1,97 @@
+import torch
+import os
+import numpy as np
+import json
+
+class myJAAD(torch.utils.data.Dataset):
+    def __init__(self, args):
+        print('Loading', args.dtype, 'data ...')
+        full_path = "/home/behnam/paper-submission/somof_data_posetrack/"
+
+        self.args = args
+        #image_size: 1280, 720
+        with open(os.path.join(full_path,"posetrack_"+args.dtype+"_frames_in.json"), 'r') as f:
+            self.frames_in = json.load(f)
+        #(306, 16)
+        with open(os.path.join(full_path,"posetrack_"+args.dtype+"_masks_in.json"), 'r') as f:
+            self.masks_in = json.load(f)
+
+        with open(os.path.join(full_path,"posetrack_"+args.dtype+"_in.json"), 'r') as f:
+            self.data_in = json.load(f)
+
+        frames_out = []
+        for i in range(len(self.frames_in)):
+           frames_out.append([])
+           path = "/".join(self.frames_in[i][-1].split("/")[:-1])
+           last = int(self.frames_in[i][-1].split("/")[-1].split(".")[0]) + 1
+           for j in range(14):
+              frames_out[i].append(path+"/{:06d}.jpg".format(last+j))
+        self.frames_out = frames_out
+
+        with open(os.path.join(full_path,"posetrack_"+args.dtype+"_masks_out.json"), 'r') as f:
+            self.masks_out = json.load(f)
+
+        with open(os.path.join(full_path,"posetrack_"+args.dtype+"_out.json"), 'r') as f:
+            self.data_out = json.load(f)
+        
+            
+
+    def __len__(self):
+        return len(self.data_in) 
+    
+    
+    def __getitem__(self, idx):
+
+        obs_p = torch.tensor(self.data_in[idx])
+        obs_m = torch.tensor(self.masks_in[idx])
+        obs_p[ torch.repeat_interleave(obs_m, 2, dim=-1) == 0 ] = np.NaN 
+        obs_s = obs_p[:,1:,:] - obs_p[:,:-1,:]
+        n_ped, seq_len, l = obs_s.shape
+
+        target_p = torch.tensor(self.data_out[idx])
+        target_m = torch.tensor(self.masks_out[idx])
+        target_p[ torch.repeat_interleave(target_m, 2, dim=-1) == 0 ] = np.NaN
+        target_s = torch.cat(((target_p[:,0,:]-obs_p[:,-1,:]).unsqueeze(1), target_p[:,1:,:]-target_p[:,:-1,:]), dim=1) 
+        n_ped, seq_len, l = target_s.shape
+        
+        obs_s[torch.isnan(obs_s)] = 0
+        obs_p[torch.isnan(obs_p)] = 0
+        target_s[torch.isnan(target_s)] = 0
+        target_p[torch.isnan(target_p)] = 0
+
+        return obs_p, obs_s, self.frames_in[idx], obs_m, target_p, target_s, self.frames_out[idx], target_m 
+
+
+def my_collate(batch):
+
+    (obs_p, obs_s, obs_f, obs_m, target_p, target_s, target_f, target_m) = zip(*batch)
+    _len = [len(seq) for seq in obs_p]
+    cum_start_idx = [0] + np.cumsum(_len).tolist()
+    seq_start_end = [[start, end]
+                     for start, end in zip(cum_start_idx[:-1], cum_start_idx[1:])]
+
+    obs_ff = []
+    target_ff = []
+    for i in range(len(_len)):
+       for j in range(_len[i]):
+          obs_ff.append(obs_f[i])
+          target_ff.append(target_f[i])
+    obs_p = torch.cat(obs_p, dim=0).permute(1,0,2)
+    obs_s = torch.cat(obs_s, dim=0).permute(1,0,2)
+    obs_m = torch.cat(obs_m, dim=0).permute(1,0,2)
+    target_p = torch.cat(target_p, dim=0).permute(1,0,2)
+    target_s = torch.cat(target_s, dim=0).permute(1,0,2)
+    target_m = torch.cat(target_m, dim=0).permute(1,0,2)
+    seq_start_end = torch.LongTensor(seq_start_end)
+    out = [obs_p, obs_s, obs_ff, obs_m, target_p, target_s, target_ff, target_m, seq_start_end]
+    return tuple(out)
+
+    
+def data_loader(args):
+    dataset = myJAAD(args)
+    dataloader = torch.utils.data.DataLoader(
+        dataset, batch_size=args.batch_size, shuffle=args.loader_shuffle,
+        pin_memory=args.pin_memory, num_workers=args.loader_workers, collate_fn=my_collate, drop_last=False)
+
+    return dataloader
+
---
+++ posetrack/model.py
@@ -0,0 +1,122 @@
+import torch
+import torch.nn as nn
+import numpy as np
+
+MAX_VAL = 7 
+
+class LSTM_g(nn.Module):
+    def __init__(self, embedding_dim=8, h_dim=16, num_layers=1, dropout=0.1):
+
+        super(LSTM_g, self).__init__()
+        self.embedding_dim = embedding_dim
+        self.h_dim = h_dim
+        self.num_layers = num_layers
+
+        self.embedding_fn = nn.Linear(2, embedding_dim)
+        self.encoder_g = nn.LSTM(embedding_dim, h_dim, num_layers, dropout=dropout)
+        self.decoder_g = nn.LSTM(embedding_dim, h_dim, num_layers, dropout=dropout)
+        self.hidden2g = nn.Linear(h_dim, 2)
+
+    def forward(self, global_s=None, pred_len=14):
+
+        seq_len, batch, l = global_s.shape
+
+        state_tuple_g = (torch.zeros(self.num_layers, batch, self.h_dim, device='cpu', dtype=torch.float64),
+                       torch.zeros(self.num_layers, batch, self.h_dim, device='cpu', dtype=torch.float64))
+
+        output_g, state_tuple_g = self.encoder_g(self.embedding_fn(global_s), state_tuple_g)
+
+        pred_s_g = torch.tensor([], device='cpu')
+        last_s_g = global_s[-1].unsqueeze(0) 
+        for _ in range(pred_len):
+
+             output_g, state_tuple_g = self.decoder_g(self.embedding_fn(last_s_g), state_tuple_g)
+             curr_s_g = self.hidden2g(output_g.view(-1, self.h_dim))
+             pred_s_g = torch.cat((pred_s_g, curr_s_g.unsqueeze(0)), dim=0)
+             last_s_g = curr_s_g.unsqueeze(0)
+
+        return pred_s_g
+
+
+
+class Encoder(nn.Module):
+    def __init__(self, pose_dim=28, h_dim=32, latent_dim=16, num_layers=2, dropout=0.2):
+        super(Encoder, self).__init__()
+
+        self.pose_dim = pose_dim
+        self.h_dim = h_dim
+        self.latent_dim = latent_dim
+        self.num_layers = num_layers
+
+        self.encoder = nn.LSTM(pose_dim, h_dim, num_layers, dropout=dropout)
+        self.FC_mean = nn.Linear(h_dim, latent_dim)
+        self.FC_var = nn.Linear(h_dim, latent_dim)
+
+    def forward(self, obs_s=None):
+
+        batch = obs_s.size(1)
+        state_tuple = (torch.zeros(self.num_layers, batch, self.h_dim, device='cpu', dtype=torch.float64),
+                       torch.zeros(self.num_layers, batch, self.h_dim, device='cpu', dtype=torch.float64))
+        output, state_tuple = self.encoder(obs_s, state_tuple)
+        out = output[-1]
+        mean = self.FC_mean(out)
+        log_var = self.FC_var(out)  
+        return mean, log_var
+
+class Decoder(nn.Module):
+    def __init__(self, pose_dim=28, h_dim=32, latent_dim=16, num_layers=1, dropout=0.2):
+        super(Decoder, self).__init__()
+        self.pose_dim = pose_dim
+        self.h_dim = h_dim
+        self.latent_dim = latent_dim
+        self.num_layers = num_layers
+
+        self.decoder = nn.LSTM(pose_dim, h_dim, num_layers, dropout=dropout)
+        self.FC = nn.Linear(latent_dim, h_dim)
+        self.mlp = nn.Linear(h_dim, pose_dim)
+
+    def forward(self, obs_s=None, latent=None, pred_len=14):
+
+        batch = obs_s.size(1)
+        decoder_c = torch.zeros(self.num_layers, batch, self.h_dim, device='cpu', dtype=torch.float64)
+        last_s = obs_s[-1].unsqueeze(0)
+        decoder_h = self.FC(latent).unsqueeze(0)
+        state_tuple = (decoder_h, decoder_c)
+
+        preds_s = torch.tensor([], device='cpu')
+        for _ in range(pred_len):
+            output, state_tuple = self.decoder(last_s, state_tuple)
+            curr_s = self.mlp(output.view(-1, self.h_dim))
+            preds_s = torch.cat((preds_s, curr_s.unsqueeze(0)), dim=0)
+            last_s = curr_s.unsqueeze(0)
+            
+        return preds_s
+
+
+class VAE(nn.Module):
+    def __init__(self, Encoder, Decoder):
+        super(VAE, self).__init__()
+        self.Encoder = Encoder
+        self.Decoder = Decoder
+
+    def reparameterization(self, mean, var):
+        epsilon = torch.randn_like(var)  # sampling epsilon
+        z = mean + var * epsilon  # reparameterization trick
+        return z
+
+    def forward(self, obs_s=None):
+        mean, log_var = self.Encoder(obs_s=obs_s)
+        z = self.reparameterization(mean, torch.exp(0.5 * log_var))  # takes exponential function (log var -> var)
+        preds_s = self.Decoder(obs_s=obs_s, latent=z)
+
+        return preds_s, mean, log_var
+
+
+def vae_loss_function(x, x_hat, mean, log_var):
+    # BCE_loss = nn.BCELoss()
+    # reproduction_loss = nn.functional.binary_cross_entropy(x_hat, x, reduction='sum')
+    assert x_hat.shape == x.shape
+    reconstruction_loss = torch.mean(torch.norm(x - x_hat, dim=len(x.shape) - 1))
+    KLD = - 0.5 * torch.sum(1+ log_var - mean.pow(2) - log_var.exp())
+    return reconstruction_loss + 0.1*KLD 
+
